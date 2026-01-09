d4ca2048480233833936c1fd897f2491500a0b2d
Alex Daniel Motogna <alexdanielmotogna@gmail.com>
2026-01-09 13:22:55 +0100

    Fix job requirements for driver licenses
    - LKW Fahrer: Change E 95 to C 95
    - LKW mit Ladekran: Change E 95 to C 95 and add Autokranführerschein requirement
    - Baggerfahrer: Remove E 95 requirement

--- src/content/jobs.ts
+++ src/content/jobs.ts
@@ -18,7 +18,8 @@ export const jobs: Job[] = [
       "Wir sind ein renommiertes Transportunternehmen, welches auf Baustellen- und Tieflader-Transporte, Kran- und Baggerarbeiten, Kelleraushub und Außenanlagen, Schotter- und Humuslieferungen spezialisiert ist.",
     requirements: [
       "Führerschein C & E erforderlich",
-      "E 95 erforderlich",
+      "C 95 erforderlich",
+      "Autokranführerschein erforderlich",
       "Ausreichende Deutschkenntnisse zur Verständigung",
       "Bereitschaft zu Überstunden",
       "Praxiserfahrung am Bau von Vorteil",
@@ -38,7 +39,6 @@ export const jobs: Job[] = [
       "Wir sind ein renommiertes Transportunternehmen, welches auf Baustellen- und Tieflader-Transporte, Kran- und Baggerarbeiten, Kelleraushub und Außenanlagen, Schotter- und Humuslieferungen spezialisiert ist.",
     requirements: [
       "Führerschein B auf F erforderlich (E von Vorteil)",
-      "E 95 erforderlich",
       "Ausreichende Deutschkenntnisse zur Verständigung",
       "Bereitschaft zu Überstunden",
       "Praxiserfahrung im Bereich Aushubarbeiten und Radbagger",
@@ -58,7 +58,7 @@ export const jobs: Job[] = [
       "Wir sind ein renommiertes Transportunternehmen, welches auf Baustellen- und Tieflader-Transporte, Kran- und Baggerarbeiten, Kelleraushub und Außenanlagen, Schotter- und Humuslieferungen spezialisiert ist.",
     requirements: [
       "Führerschein C erforderlich (E von Vorteil)",
-      "E 95 erforderlich",
+      "C 95 erforderlich",
       "Ausreichende Deutschkenntnisse zur Verständigung",
       "Bereitschaft zu Überstunden",
       "Praxiserfahrung am Bau von Vorteil",
